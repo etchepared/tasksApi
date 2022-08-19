4c85f6bcccacc8c093cea402734183a207db9737
unknown <msosa89@outlook.com>
2022-08-18 23:29:43 -0300

    configuracion inical back

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 app.use(express.json()); // middleware que transforma la req.body a un json
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.get("/ping", (req, res) => {
   console.log("someone pinged here!!");
   res.send("pong");
 });
