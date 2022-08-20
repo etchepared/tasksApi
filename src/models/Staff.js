319e0709f438a746dc3098b2d29ad16fa2c9b198
etchepared <etcheparede@gmail.com>
2022-08-20 17:02:38 -0300

    model Staff created

---
+++ src/models/Staff.js
@@ -0,0 +1,30 @@
+const { Staff } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "taskmanager",
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+    },
+    { paranoid: true }
+  );
+};
