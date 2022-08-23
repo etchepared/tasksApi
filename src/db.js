9bbc7f27df4281c5f4676e8fbf05f50f28ab091a
etchepared <88214684+etchepared@users.noreply.github.com>
2022-08-22 23:35:44 -0300

    models associations (#5)
    * models associations
    
    * db associations

--- src/db.js
+++ src/db.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-} = process.env;
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 let sequelize =
   process.env.NODE_ENV === "production"
@@ -44,31 +42,55 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Project, Tech } = sequelize.models;
+const { Project, Staff, Rol } = sequelize.models;
 
 // Aca vendrian las relaciones
 
 //muchos a muchos
-Project.belongsToMany(Tech, { as: "tech", through: 'project_tech', foreignKey: "project_id" });
-Tech.belongsToMany(Project, { as: "project", through: 'project_tech', foreignKey: "tech_id" }); 
+Project.belongsToMany(Staff, {
+  as: "staff",
+  through: "project_staff",
+  foreignKey: "project_id",
+});
+Staff.belongsToMany(Project, {
+  as: "project",
+  through: "project_staff",
+  foreignKey: "staff_id",
+});
+Rol.belongsToMany(Staff, {
+  as: "staff",
+  through: "rol_staff",
+  foreignKey: "rol_id",
+});
+Staff.belongsToMany(Rol, {
+  as: "rol",
+  through: "rol_staff",
+  foreignKey: "staff_id",
+});
 
 //DESCOMENTAR ESTO
-/* module.exports = {
+module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
-}; */
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+};
